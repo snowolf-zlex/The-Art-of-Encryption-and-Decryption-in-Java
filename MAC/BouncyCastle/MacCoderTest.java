8b290ee0510120c123279f3c8458cf50ae7a70d3
Snowolf <zlex.dongliang@gmail.com>
2023-12-19 10:23:32 +0800

    Update and rename MACCoderTest.java to MacCoderTest.java

--- MAC/BouncyCastle/MacCoderTest.java
+++ MAC/BouncyCastle/MacCoderTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
  * @version 1.0
  * @since 1.0
  */
-public class MACCoderTest {
+public class MacCoderTest {
 
 	/**
 	 * 测试HmacMD2
@@ -22,11 +22,11 @@ public class MACCoderTest {
 		String str = "HmacMD2消息摘要";
 
 		// 初始化密钥
-		byte[] key = MACCoder.initHmacMD2Key();
+		byte[] key = MacCoder.initHmacMD2Key();
 
 		// 获得摘要信息
-		byte[] data1 = MACCoder.encodeHmacMD2(str.getBytes(), key);
-		byte[] data2 = MACCoder.encodeHmacMD2(str.getBytes(), key);
+		byte[] data1 = MacCoder.encodeHmacMD2(str.getBytes(), key);
+		byte[] data2 = MacCoder.encodeHmacMD2(str.getBytes(), key);
 
 		// 校验
 		assertArrayEquals(data1, data2);
@@ -43,11 +43,11 @@ public class MACCoderTest {
 		String str = "HmacMD4消息摘要";
 
 		// 初始化密钥
-		byte[] key = MACCoder.initHmacMD4Key();
+		byte[] key = MacCoder.initHmacMD4Key();
 
 		// 获得摘要信息
-		byte[] data1 = MACCoder.encodeHmacMD4(str.getBytes(), key);
-		byte[] data2 = MACCoder.encodeHmacMD4(str.getBytes(), key);
+		byte[] data1 = MacCoder.encodeHmacMD4(str.getBytes(), key);
+		byte[] data2 = MacCoder.encodeHmacMD4(str.getBytes(), key);
 
 		// 校验
 		assertArrayEquals(data1, data2);
@@ -64,11 +64,11 @@ public class MACCoderTest {
 		String str = "HmacSHA224消息摘要";
 
 		// 初始化密钥
-		byte[] key = MACCoder.initHmacSHA224Key();
+		byte[] key = MacCoder.initHmacSHA224Key();
 
 		// 获得摘要信息
-		byte[] data1 = MACCoder.encodeHmacSHA224(str.getBytes(), key);
-		byte[] data2 = MACCoder.encodeHmacSHA224(str.getBytes(), key);
+		byte[] data1 = MacCoder.encodeHmacSHA224(str.getBytes(), key);
+		byte[] data2 = MacCoder.encodeHmacSHA224(str.getBytes(), key);
 
 		// 校验
 		assertArrayEquals(data1, data2);
